59ba2fcd10f429aec82366172429ca2bb9a61491
pwiener <patryk.wiener@siroccomobile.com>
2021-11-22 18:56:55 +0100

    wallet implementation

--- sample_2_wallet/wallet.py
+++ sample_2_wallet/wallet.py
@@ -1,22 +1,37 @@
-class InsufficientAmount(Exception):
+class InsufficientAmountError(Exception):
     pass
 
 
 class Wallet:
 
     def __init__(self, amount=0):
-        self.balance = 0  # public
-        self._balance = 0  # protected
-        self.__balance = 0  # private
+        self._balance = amount  # protected
 
     def _validate_operation(self, spending):
-        pass
+        if self._balance < spending:
+            raise InsufficientAmountError
 
     def add_cash(self, cash):
-        pass
+        self._balance += cash
 
     def spend_cash(self, spending):
-        pass
+        self._validate_operation(spending=spending)
+        self._balance -= spending
 
     def get_balance(self):
-        pass
+        return self._balance
+
+
+if __name__ == '__main__':
+    wallet = Wallet(amount=10)
+    print(wallet.get_balance())
+    wallet.add_cash(cash=10)
+    print(wallet.get_balance())
+    wallet.spend_cash(spending=10)
+    print(wallet.get_balance())
+    wallet.spend_cash(spending=20)
+    print(wallet.get_balance())
+
+    empty_wallet = Wallet()
+    t = 0
+
