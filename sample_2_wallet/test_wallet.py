adad93a8224a84f1395ed73b2f3d5dd8172676be
pwiener <patryk.wiener@siroccomobile.com>
2021-11-22 20:52:35 +0100

    applied fixtures

--- sample_2_wallet/test_wallet.py
+++ sample_2_wallet/test_wallet.py
@@ -4,48 +4,42 @@ from sample_2_wallet.wallet import Wallet, InsufficientAmountError
 
 
 class TestWallet:
+    AMOUNT = 10
+    SPENDING = 10
+
+    def setup_method(self):
+        self.wallet = Wallet(amount=self.AMOUNT)
+        self.empty_wallet = Wallet()
 
     def test_default_initial_amount(self):
-        wallet = Wallet()
         expected = 0
 
-        actual = wallet.get_balance()
+        actual = self.empty_wallet.get_balance()
 
         assert expected == actual
 
     def test_setting_initial_amount(self):
-        amount = 10
-        wallet = Wallet(amount=amount)
-        expected = 10
+        actual = self.wallet.get_balance()
 
-        actual = wallet.get_balance()
-
-        assert expected == actual
+        assert self.AMOUNT == actual
 
     def test_spend_cash(self):
-        amount = 10
-        wallet = Wallet(amount=amount)
-        spending = 10
         expected = 0
 
-        wallet.spend_cash(spending=spending)
-        actual = wallet.get_balance()
+        self.wallet.spend_cash(spending=self.SPENDING)
+        actual = self.wallet.get_balance()
 
         assert expected == actual
 
     def test_add_cash(self):
-        amount = 10
-        wallet = Wallet(amount=amount)
         cash = 10
         expected = 20
 
-        wallet.add_cash(cash=cash)
-        actual = wallet.get_balance()
+        self.wallet.add_cash(cash=cash)
+        actual = self.wallet.get_balance()
 
         assert expected == actual
 
     def test_spend_cash_raises_exception_on_insufficient_amount(self):
-        wallet = Wallet()
-        spending = 10
         with pytest.raises(InsufficientAmountError):
-            wallet.spend_cash(spending=spending)
+            self.empty_wallet.spend_cash(spending=self.SPENDING)
