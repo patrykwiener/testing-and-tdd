ce0353f383d01dd5d14872e06485acf19af96f82
pwiener <patryk.wiener@siroccomobile.com>
2021-11-23 21:04:34 +0100

    owner tests with mocks

--- sample_5_mocks/explanation/sample_mocks.py
+++ sample_5_mocks/explanation/sample_mocks.py
@@ -1,28 +1,44 @@
 from unittest.mock import Mock, MagicMock
 
-# class Sample:
-#     def __init__(self):
-#         self.sample_str = ...
-#         self.sample_number = ...
-#
-#     def some_method(self):
-#         pass
+
+class Sample:
+    def __init__(self):
+        self.sample_str = ...
+        self.sample_number = ...
+
+    def some_method(self):
+        pass
+
 
 def mock1():
-    # sample = Sample()
-    # sample.sample_str
-    # sample.sample_number
-    #
+    sample = Sample()
+    sample.sample_str
+    sample.sample_number
+
     mock = Mock()
     mock.sample_str = 'sample_str'
+    mock.ala_ma_kota = 'sample_str'
     mock.sample_number = 10
+
+    print(mock.sample_number)
+    print(mock.sample_str)
+
     mock.some_method()
-    t=0
+    t = 0
+
+
+def add_number(num1, num2):
+    return num1 + num2
 
 
 def mock2():
-    mock = Mock(return_value='mock_value')
-    assert mock.return_value == 'mock_value'
+    mock = Mock(return_value=20)
+    mock(15, 5)
+    assert mock.return_value == 20
+    assert mock() == 20
+
+    mock.get_balance.return_value = 20
+    mock.get_balance()
 
     mock.some_method = Mock(return_value='mocked_method_value')
     assert mock.some_method() == 'mocked_method_value'
@@ -51,9 +67,12 @@ if __name__ == '__main__':
     mock1()
     mock2()
     # mock3()
-    magic_mock1()
-    b = Mock()
-    mock = MagicMock()
+    # magic_mock1()
+    # b = Mock()
 
+    mock = MagicMock()
     mock('test', 123)
-    mock.assert_called_with('123', 123)
+    mock.assert_called()
+    mock.assert_called_with('test', 123)
+    mock.assert_called_once()
+    mock.assert_called_once_with('test', 123)
--- sample_5_mocks/tests/test_owner.py
+++ sample_5_mocks/tests/test_owner.py
@@ -1,6 +1,9 @@
 from unittest.mock import MagicMock
 
+import pytest
+
 from sample_5_mocks.owner import Owner
+from sample_5_mocks.wallet import InsufficientAmountError
 
 
 class TestOwner:
@@ -9,19 +12,56 @@ class TestOwner:
 
     def setup_method(self) -> None:
         self.wallet_mock = MagicMock()  # == Wallet()
-        self.owner = Owner(self.FIRST_NAME, self.LAST_NAME, self.wallet_mock)
+        self.owner = Owner(first_name=self.FIRST_NAME, last_name=self.LAST_NAME, wallet=self.wallet_mock)
 
     def test_owner_first_name(self):
-        pass
+        assert self.owner.first_name == self.FIRST_NAME
 
     def test_owner_last_name(self):
-        pass
+        assert self.owner.last_name == self.LAST_NAME
 
     def test_owner_supply_to_wallet(self):
-        pass
+        cash = 10
+
+        self.owner.supply_wallet(cash=cash)
+
+        self.wallet_mock.add_cash.assert_called_once_with(cash=cash)
 
     def test_owner_withdraw_money(self):
-        pass
+        amount = 10
+
+        self.owner.withdraw_money(amount=amount)
+
+        self.wallet_mock.spend_cash.assert_called_once_with(spending=amount)
 
     def test_owner_check_if_can_afford_true(self):
-        pass
+        amount = 10
+        self.wallet_mock.get_balance.return_value = 20
+        # self.wallet_mock.get_balance = MagicMock(return_value=20)
+
+        result = self.owner.check_if_can_afford(amount=amount)
+
+        # assert result is True
+        assert result
+        self.wallet_mock.get_balance.assert_called_once()
+
+    def test_owner_check_if_can_afford_false(self):
+        amount = 30
+        self.wallet_mock.get_balance.return_value = 20
+        # self.wallet_mock.get_balance = MagicMock(return_value=20)
+
+        result = self.owner.check_if_can_afford(amount=amount)
+
+        # assert result is False
+        assert not result
+        self.wallet_mock.get_balance.assert_called_once()
+
+    def test_owner_withdraw_money_raises_exception(self):
+        amount = 10
+        # self.wallet_mock.spend_cash = MagicMock(side_effect=InsufficientAmountError)
+        self.wallet_mock.spend_cash.side_effect = InsufficientAmountError
+
+        with pytest.raises(InsufficientAmountError):
+            self.owner.withdraw_money(amount=amount)
+
+        self.wallet_mock.spend_cash.assert_called_once_with(spending=amount)
